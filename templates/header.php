d2bbfe1cc96e519e19b2bdf33bb7c69badcd76e2
Metallizer <thomas.noury@is06.com>
2012-02-10 23:45:20 +0100

    Integration du bandeau en cours (1/2)

--- templates/header.php
+++ templates/header.php
@@ -9,29 +9,35 @@
     <div class="body_container">
       <header>
         <div class="header">
-          <div class="logo">
-            <a href="#">Zelda Solarus : création de jeux Zelda amateurs</a>
+          <div class="top_bar"></div>
+          <div class="logo_space">
+            <div class="logo">
+              <a href="accueil.php" title="Accueil du site"><span>Zelda Solarus : création de jeux Zelda amateurs</span></a>
+            </div>
+            <div class="character"></div>
+            <div class="search">
+              <form method="post" action="recherche_resultats.php">
+                <input type="text" value="Rechercher" />
+                <input type="submit" value="OK" />
+              </form>
+            </div>
+            <div class="clear"></div>
           </div>
-          <div class="search">
-            <form method="post" action="recherche_resultats.php">
-              <input type="text" value="Rechercher" />
-              <input type="submit" value="OK" />
-            </form>
+          <div class="nav">
+            <div class="left"></div>
+            <ul>
+              <li><a href="accueil.php">Accueil</a></li>
+              <li><a href="jeu_liste.php">Jeux</a></li>
+              <li><a href="solution_liste.php">Solutions</a></li>
+              <li><a href="dossier_liste.php">Dossiers</a></li>
+              <li><a href="global_telechargement.php">Téléchargements</a></li>
+              <li><a href="">Forums</a></li>
+              <li><a href="contenu_generique.php">Equipe</a></li>
+            </ul>
+            <div class="clear"></div>
           </div>
+          <div class="bottom_bar"></div>
         </div>
       </header>
       
-      <div class="nav">
-        <nav>
-          <ul>
-            <li><a href="accueil.php">Accueil</a></li>
-            <li><a href="jeu_liste.php">Jeux</a></li>
-            <li><a href="solution_liste.php">Solutions</a></li>
-            <li><a href="dossier_liste.php">Dossiers</a></li>
-            <li><a href="global_telechargement.php">Téléchargements</a></li>
-            <li><a href="">Forums</a></li>
-            <li><a href="contenu_generique.php">Equipe</a></li>
-          </ul>
-          <div class="clear"></div>
-        </nav>
-      </div>
+      
