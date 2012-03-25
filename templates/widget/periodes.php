7dd5230d2d979b40611cc265641394d895c7ad56
is06 <thomas.noury@is06.com>
2012-03-25 20:30:06 +0200

    Archives des news ajoutées

---
+++ templates/widget/periodes.php
@@ -0,0 +1,14 @@
+<div class="sidebox sb_style_std">
+  <h2>Filtrer par période</h2>
+  <div class="sb_content forme">
+    <ul>
+      <li><a href="">Janvier 2012</a></li>
+      <li><a href="">Décembre 2011</a></li>
+      <li><a href="">Novembre 2011</a></li>
+      <li><a href="">Octobre 2011</a></li>
+      <li><a href="">Septembre 2011</a></li>
+      <li><a href="">Août 2011</a></li>
+      <li><a href="">Juillet 2011</a></li>
+    </ul>
+  </div>
+</div>
