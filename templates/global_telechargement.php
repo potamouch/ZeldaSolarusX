d11646d94a272c2a4151e3348478f3d4b2785978
is06 <thomas.noury@is06.com>
2012-04-04 14:44:05 +0200

    Finalisation des téléchargements (global et jeux)

--- templates/global_telechargement.php
+++ templates/global_telechargement.php
@@ -5,18 +5,42 @@
 <div class="main_content">
   <h2>Derniers téléchargements disponibles</h2>
 
+  <div class="forme introduction">
+    <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a>. Fusce pharetra metus vel <strong>est laoreet rhoncus</strong>. In hac habitasse platea dictumst. Nulla id nibh ut urna elementum elementum vitae eget felis. <em>Duis pretium mauris at tellus tempus et mollis quam posuere</em>. Maecenas nisi mi, posuere nec fermentum at, condimentum ut massa.</p>
+  </div>
+
   <ul class="last_downloads">
     <!-- Début élément téléchargement -->
     <li>
       <div class="download_info">
         <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
-        <p>Description du téléchargement</p>
+        <p>Description du téléchargement lorem ipsum blablabla dolor sit amet consectetuer.</p>
         <ul class="file_info">
+          <li>Système : <strong class="systeme systeme_windows">Windows</strong></li>
           <li>Format : <strong>ZIP</strong></li>
-          <li>Nombre de téléchargements : <strong>1178</strong></li>
         </ul>
       </div>
-      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="bouton bouton_right">
+          <a href="">Télécharger</a>
+          <span class="downloaded">Nombre : <strong>1178</strong></span>
+        </div>
+      <div class="clear"></div>
+    </li>
+    <!-- Fin élément téléchargement -->
+
+    <!-- Début élément téléchargement -->
+    <li>
+      <div class="download_info">
+        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
+        <ul class="file_info">
+          <li>Système : <strong class="systeme systeme_debian">Debian / Ubuntu</strong></li>
+          <li>Format : <strong>ZIP</strong></li>
+        </ul>
+      </div>
+      <div class="bouton bouton_right">
+          <a href="">Télécharger</a>
+          <span class="downloaded">Nombre : <strong>1178</strong></span>
+        </div>
       <div class="clear"></div>
     </li>
     <!-- Fin élément téléchargement -->
@@ -27,10 +51,12 @@
         <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
         <ul class="file_info">
           <li>Format : <strong>ZIP</strong></li>
-          <li>Nombre de téléchargements : <strong>1178</strong></li>
         </ul>
       </div>
-      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="bouton bouton_right">
+          <a href="">Télécharger</a>
+          <span class="downloaded">Nombre : <strong>1178</strong></span>
+        </div>
       <div class="clear"></div>
     </li>
     <!-- Fin élément téléchargement -->
@@ -41,24 +67,12 @@
         <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
         <ul class="file_info">
           <li>Format : <strong>ZIP</strong></li>
-          <li>Nombre de téléchargements : <strong>1178</strong></li>
         </ul>
       </div>
-      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
-      <div class="clear"></div>
-    </li>
-    <!-- Fin élément téléchargement -->
-
-    <!-- Début élément téléchargement -->
-    <li>
-      <div class="download_info">
-        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
-        <ul class="file_info">
-          <li>Format : <strong>ZIP</strong></li>
-          <li>Nombre de téléchargements : <strong>1178</strong></li>
-        </ul>
-      </div>
-      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="bouton bouton_right">
+          <a href="">Télécharger</a>
+          <span class="downloaded">Nombre : <strong>1178</strong></span>
+        </div>
       <div class="clear"></div>
     </li>
     <!-- Fin élément téléchargement -->
