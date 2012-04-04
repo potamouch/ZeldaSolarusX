523b2e58ac070f9c859e666768ac750ec5ad72b0
is06 <thomas.noury@is06.com>
2012-04-04 14:19:55 +0200

    Page de téléchargement globale

--- templates/global_telechargement.php
+++ templates/global_telechargement.php
@@ -2,4 +2,74 @@
 
 <h1>Téléchargements</h1>
 
+<div class="main_content">
+  <h2>Derniers téléchargements disponibles</h2>
+
+  <ul class="last_downloads">
+    <!-- Début élément téléchargement -->
+    <li>
+      <div class="download_info">
+        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
+        <p>Description du téléchargement</p>
+        <ul class="file_info">
+          <li>Format : <strong>ZIP</strong></li>
+          <li>Nombre de téléchargements : <strong>1178</strong></li>
+        </ul>
+      </div>
+      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="clear"></div>
+    </li>
+    <!-- Fin élément téléchargement -->
+
+    <!-- Début élément téléchargement -->
+    <li>
+      <div class="download_info">
+        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
+        <ul class="file_info">
+          <li>Format : <strong>ZIP</strong></li>
+          <li>Nombre de téléchargements : <strong>1178</strong></li>
+        </ul>
+      </div>
+      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="clear"></div>
+    </li>
+    <!-- Fin élément téléchargement -->
+
+    <!-- Début élément téléchargement -->
+    <li>
+      <div class="download_info">
+        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
+        <ul class="file_info">
+          <li>Format : <strong>ZIP</strong></li>
+          <li>Nombre de téléchargements : <strong>1178</strong></li>
+        </ul>
+      </div>
+      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="clear"></div>
+    </li>
+    <!-- Fin élément téléchargement -->
+
+    <!-- Début élément téléchargement -->
+    <li>
+      <div class="download_info">
+        <h3>Démo 1.4 de Zelda : Mystery of Solarus DX</h3>
+        <ul class="file_info">
+          <li>Format : <strong>ZIP</strong></li>
+          <li>Nombre de téléchargements : <strong>1178</strong></li>
+        </ul>
+      </div>
+      <div class="bouton bouton_right"><a href="">Télécharger - 11,8 Mo</a></div>
+      <div class="clear"></div>
+    </li>
+    <!-- Fin élément téléchargement -->
+  </ul>
+
+  <h2>Téléchargements de nos jeux</h2>
+  <ul class="global_downloads">
+    <li><a href="jeu_telechargement.php">Zelda : Mystery of Solarus DX</a></li>
+    <li><a href="jeu_telechargement.php">Zelda : Mystery of Solarus XD</a></li>
+    <li><a href="jeu_telechargement.php">Zelda : Mercuris' Chest</a></li>
+  </ul>
+</div>
+
 <?php include('footer.php'); ?>
