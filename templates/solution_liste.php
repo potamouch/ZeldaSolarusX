84c48850a574c7b536a28a07ab48cc26472f2bf1
is06 <thomas.noury@is06.com>
2012-04-23 11:57:33 +0200

    Liste des solutions du site

--- templates/solution_liste.php
+++ templates/solution_liste.php
@@ -3,7 +3,61 @@
 <h1>Solutions des jeux</h1>
 
 <div class="main_content">
-  <h2><a href="solution_sommaire.php">Zelda : Mystery of Solarus DX</a></h2>
+
+  <!-- Petite phrase explicative de la page des solutions -->
+  <div class="forme introduction">
+    <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a>. Fusce pharetra metus vel <strong>est laoreet rhoncus</strong>. In hac habitasse platea dictumst. Nulla id nibh ut urna elementum elementum vitae eget felis. <em>Duis pretium mauris at tellus tempus et mollis quam posuere</em>. Maecenas nisi mi, posuere nec fermentum at, condimentum ut massa.</p>
+  </div>
+
+  <ul class="solution_liste">
+    <!-- Solution avec description -->
+    <li>
+      <div class="sol_image"><img src="www_images/dungeon_8_pots.png" alt="" width="134" height="100" /></div>
+      <div class="sol_contenu">
+        <h2><a href="solution_sommaire.php">Solution de Zelda : Mystery of Solarus DX</a></h2>
+        <p class="forme">Petite description de la solution Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a>. Fusce pharetra metus vel <strong>est laoreet rhoncus</strong>. In hac habitasse platea dictumst. Nulla id nibh ut urna elementum elementum vitae eget felis. <em>Duis pretium mauris at tellus tempus et mollis quam posuere</em>. Maecenas nisi mi, posuere nec fermentum at, condimentum ut massa.</p>
+        <div class="info">Statut : <strong>en cours</strong> - Dernière mise à jour : <strong>23/04/2012</strong></div>
+        <div class="plus"><a href="solution_sommaire.php">Consulter la solution</a></div>
+      </div>
+      <div class="clear"></div>
+    </li>
+
+    <!-- Solution sans description -->
+    <li>
+      <div class="sol_image"><img src="www_images/dungeon_8_pots.png" alt="" width="134" height="100" /></div>
+      <div class="sol_contenu">
+        <h2><a href="solution_sommaire.php">Solution de Zelda : Mystery of Solarus DX</a></h2>
+        <div class="info">Statut : <strong>complète</strong> - Dernière mise à jour : <strong>23/04/2012</strong></div>
+        <div class="plus"><a href="solution_sommaire.php">Consulter la solution</a></div>
+      </div>
+      <div class="clear"></div>
+    </li>
+
+    <!-- Solution avec description -->
+    <li>
+      <div class="sol_image"><img src="www_images/dungeon_8_pots.png" alt="" width="134" height="100" /></div>
+      <div class="sol_contenu">
+        <h2><a href="solution_sommaire.php">Solution de Zelda : Mystery of Solarus DX</a></h2>
+        <p class="forme">Petite description de la solution Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a>. Fusce pharetra metus vel <strong>est laoreet rhoncus</strong>. In hac habitasse platea dictumst. Nulla id nibh ut urna elementum elementum vitae eget felis. <em>Duis pretium mauris at tellus tempus et mollis quam posuere</em>. Maecenas nisi mi, posuere nec fermentum at, condimentum ut massa.</p>
+        <div class="info">Statut : <strong>en cours</strong> - Dernière mise à jour : <strong>23/04/2012</strong></div>
+        <div class="plus"><a href="solution_sommaire.php">Consulter la solution</a></div>
+      </div>
+      <div class="clear"></div>
+    </li>
+
+    <!-- Solution avec description -->
+    <li>
+      <div class="sol_image"><img src="www_images/dungeon_8_pots.png" alt="" width="134" height="100" /></div>
+      <div class="sol_contenu">
+        <h2><a href="solution_sommaire.php">Solution de Zelda : Mystery of Solarus DX</a></h2>
+        <p class="forme">Petite description de la solution Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a>. Fusce pharetra metus vel <strong>est laoreet rhoncus</strong>. In hac habitasse platea dictumst. Nulla id nibh ut urna elementum elementum vitae eget felis. <em>Duis pretium mauris at tellus tempus et mollis quam posuere</em>. Maecenas nisi mi, posuere nec fermentum at, condimentum ut massa.</p>
+        <div class="info">Statut : <strong>en cours</strong> - Dernière mise à jour : <strong>23/04/2012</strong></div>
+        <div class="plus"><a href="solution_sommaire.php">Consulter la solution</a></div>
+      </div>
+      <div class="clear"></div>
+    </li>
+  </ul>
+
 </div>
 
 <?php include('footer.php'); ?>
