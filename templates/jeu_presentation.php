8e39a111d99fbe198b84c71ecf0d29e019af11bd
is06 <thomas.noury@is06.com>
2012-05-22 22:07:47 +0200

    Liste des articles en cours

--- templates/jeu_presentation.php
+++ templates/jeu_presentation.php
@@ -2,10 +2,37 @@
 
 <h1>Zelda : Mystery of Solarus DX</h1>
 
-<ul>
-  <li><a href="image_liste.php">Images</a></li>
-  <li><a href="jeu_telechargement.php">Téléchargements</a></li>
-  <li><a href="solution_sommaire.php">Solution</a></li>
-</ul>
+<div class="main_content">
+
+  <div class="jeu_bandeau">
+    <div class="logo">
+      <img src="" alt="Logo de Zelda : Mystery of Solarus DX" width="" height="" />
+    </div>
+    <div class="synopsis" class="forme">
+      <p>Texte descriptif du site. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur lorem quis urna auctor id suscipit velit vulputate. Ut pharetra mollis ante, nec lacinia lacus pretium vitae. Aliquam erat volutpat.</p>
+    </div>
+    
+  </div>
+  
+  <div class="bloc_gauche_2_3">
+    <ul>
+      <li><a href="image_liste.php">Images</a></li>
+      <li><a href="jeu_telechargement.php">Téléchargements</a></li>
+      <li><a href="solution_sommaire.php">Solution</a></li>
+    </ul>
+  </div>
+  
+  <div class="bloc_droite_1_3">
+    
+    <!-- Side bar -->
+    <aside>
+      <div class="sidebar">
+        <?php include('widget/rubriques.php'); ?>
+      </div>
+    </aside>
+    
+  </div>
+  
+  <div class="clear"></div>
 
 <?php include('footer.php'); ?>
